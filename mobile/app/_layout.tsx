c1e8572df96e9d7f81e958a93742cd48047bb4be
temevh <temehama@gmail.com>
2025-11-15 17:53:47 +0200

    aa

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,11 +2,8 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-<<<<<<< HEAD
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-=======
 import { UserProvider } from '@/contexts/UserContext';
->>>>>>> ced609b (added context)
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { ThemeProvider as CustomThemeProvider } from '@/contexts/ThemeContext';
@@ -19,27 +16,18 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-<<<<<<< HEAD
     <SafeAreaProvider>
       <CustomThemeProvider>
         <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-=======
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <UserProvider>
->>>>>>> ced609b (added context)
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(tabs)" />
-            <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-          </Stack>
-<<<<<<< HEAD
-          <StatusBar style="auto" />
+          <UserProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="(tabs)" />
+              <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+            </Stack>
+            <StatusBar style="auto" />
+          </UserProvider>
         </ThemeProvider>
       </CustomThemeProvider>
     </SafeAreaProvider>
-=======
-        <StatusBar style="auto" />
-      </UserProvider >
-    </ThemeProvider>
->>>>>>> ced609b (added context)
   );
 }
